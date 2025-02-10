340c9cf635b7dfda0ea0482ee6d73f582eb8d1e9
StefanSirkovic <stefansirkovic@gmail.com>
2025-02-10 10:45:01 +0100

    Add spring initializr files

---
+++ backend/finance-tracker/src/main/java/com/spring/finance_tracker/SpringFinanceTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.spring.finance_tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringFinanceTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringFinanceTrackerApplication.class, args);
+	}
+
+}
---
+++ backend/finance-tracker/src/test/java/com/spring/finance_tracker/SpringFinanceTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.finance_tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringFinanceTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
